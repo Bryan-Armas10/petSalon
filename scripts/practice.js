a165ce23dc6c7b90040e2c08fc20301f379d8cf8
Bryan-Armas10 <bryan.armas@sdgku.edu>
2024-10-08 19:40:43 -0700

    object literal

---
+++ scripts/practice.js
@@ -0,0 +1,8 @@
+// object literal
+let student1 ={
+    name:"John Smith",
+    age:99,
+    isAdmin:false,
+    grade1:3.7
+}
+console.log(student1.isAdmin);
---
+++ scripts/register.js
@@ -0,0 +1,23 @@
+console.log("register.js");
+let pets = []; // empty array
+
+// Creation pets
+let pet1 = {
+    name:"Scooby",
+    age:99,
+    gender:"Male"
+}
+
+
+
+pets.push(pet1);
+
+function displayNames(){
+    // display pet names
+    console.log(pets[0].name);
+    /// console.log(pets[1].name);
+
+// hint: use a for loop and travel the array
+// hint: use brute force\
+// hint: length the ampunt of elements on the array
+}
