550410c9baef4a60e68dfacd0b88d417d03db980
Bryan-Armas10 <bryan.armas@sdgku.edu>
2024-10-09 19:09:13 -0700

    object literal corection

--- scripts/register.js
+++ scripts/register.js
@@ -5,19 +5,39 @@ let pets = []; // empty array
 let pet1 = {
     name:"Scooby",
     age:99,
-    gender:"Male"
+    gender:"Male",
+    service:"shower",
+    breed:"boxer"
+}
+let pet2 = {
+    name:"Snoopy",
+    age:10,
+    gender:"Male",
+    service:"shower",
+    breed:"Dalmatian"
+}
+let pet3 = {
+    name:"Marley",
+    age:20,
+    gender:"Female",
+    service:"shower",
+    breed:"chihuahua"
 }
 
-
-
 pets.push(pet1);
+pets.push(pet2);
+pets.push(pet3);
 
 function displayNames(){
     // display pet names
     console.log(pets[0].name);
+    console.log(pets[1].name);
+    console.log(pets[2].name);
     /// console.log(pets[1].name);
 
 // hint: use a for loop and travel the array
 // hint: use brute force\
 // hint: length the ampunt of elements on the array
 }
+
+displayNames();
