389b46563927f2cf79ee0da117261b8739fdcde0
Bryan-Armas10 <bryan.armas@sdgku.edu>
2024-10-11 20:37:35 -0700

    assignments2

--- scripts/register.js
+++ scripts/register.js
@@ -1,40 +1,41 @@
 console.log("register.js");
 let pets = []; // empty array
 
-// Creation pets
-let pet1 = {
-    name:"Scooby",
-    age:99,
-    gender:"Male",
-    service:"Grooming",
-    breed:"boxer"
+function Pet(name,age,gender,breed,service){
+    // properties = parameter (value) 
+    this.name=name;
+    this.age=age;
+    this.gender=gender;
+    this.breed=breed;
+    this.service=service;
 }
-let pet2 = {
-    name:"Snoopy",  
-    age:10,
-    gender:"Male",
-    service:"Grooming",
-    breed:"Dalmatian"
-}   
-let pet3 = {
-    name:"Marley",
-    age:20,
-    gender:"Female",
-    service:"Grooming",
-    breed:"chihuahua"
-}
-// Push the pets
-pets.push(pet1,pet2,pet3);
 
-// Function displayCount is for total pets register
-function displayCount() {
-    let count = pets.length;
-    document.getElementById("petCount").innerHTML = `<h2>Total pets: ${count}</h2>`;
+function register(){
+    let inputName = document.getElementById("txtName").value;
+    let inputAge = document.getElementById("txtAge").value;
+    // let inputGender = document.getElementById("txtGender").value;
+    let inputGender = document.querySelector('input[name="gender"]:checked')?.value || '';
+    let inputBreed = document.getElementById("txtBreed").value;
+    let inputService = document.getElementById("txtService").value;
+
+    console.log(inputName,inputAge,inputGender,inputBreed,inputService);
+    // Create de object
+    let newPet = new Pet(inputName,inputAge,inputGender,inputBreed,inputService);
+    console.log(newPet);
+    pets.push(newPet);
 }
-// Function displayNames is for names the all pets
-function displayNames(){
-    for(let i = 0; i < pets.length; i++){
-        document.getElementById("petnames").innerHTML +=`<p>name: ${pets[i].name}</p>`;
-    }
-    console.log(pets.length);
+
+function init(){
+    console.log("init");
+    let pet1 = new Pet("Scooby",99,"Male","Grooming");
+    // create 2 more pets
+    let pet2 = new Pet("Snoopy",30,"Male","Grooming");
+    let pet3 = new Pet("Tuti",50,"Female","Grooming");
+
+    // push the pets on the array
+    pets.push(pet1,pet2,pet3);
+    //display the three pets on the console
+    console.log(pets);
 }
+
+window.onload=init; // wait to render the html  
